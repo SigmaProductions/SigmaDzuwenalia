4f75ec68a1e440b7e90e55e72f443bb3d81797ec
MacenDi <mstanekofficial@gmail.com>
2018-11-16 22:51:47 +0100

    DBContextAdded

---
+++ Backend/SigmaDzuwenalia/SigmaDzuwenalia.DataAccess/Context/IDzuwenaliaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using SigmaDzuwenalia.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaDzuwenalia.DataAccess.Context
+{
+    public interface IDzuwenaliaDBContext: IDisposable
+    {
+        IDbSet<FlankiEntity> Flanki { get; set; }
+        
+        Task SaveChanges();
+    }
+}
