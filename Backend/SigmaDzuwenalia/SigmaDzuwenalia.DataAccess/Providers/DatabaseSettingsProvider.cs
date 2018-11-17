7eaf615bf067850072767c8672632ece81e3dba7
username2468 <zata@xcodes.net>
2018-11-17 02:14:17 +0100

    sd

---
+++ Backend/SigmaDzuwenalia/SigmaDzuwenalia.DataAccess/Providers/DatabaseSettingsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaDzuwenalia.DataAccess.Providers
+{
+    public class DatabaseSettingsProvider : IDatabaseSettingsProvider
+    {
+        public DatabaseConnectionSettings GetSettings()
+        {
+            
+            var connection = @"Server=ASPIRE\SQLEXPRESS;Database=DzuwenaliaDB;Trusted_Connection=True;";
+            return new DatabaseConnectionSettings(connection);
+        }
+    }
+}
