4ca2137ed4a3f9e4920c5d665461f171ea782220
MacenDi <mstanekofficial@gmail.com>
2018-11-17 08:54:58 +0100

    HelperAdded

---
+++ frontend/Helpersome/Whatever/Whatever/Police.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever
+{
+    class PolicePickle
+    {
+        public int Id;
+        public double XCoordinate;
+        public double YCoordinate;
+        public int PatrolSize;
+        public DateTime PatrolDate;
+    }
+
+    static class ConnectionHelperPolice
+    {
+        public static void SendPolice(LatLng point)
+        {
+            var t = new PolicePickle();
+            t.Id = 0;
+            t.XCoordinate = point.Latitude;
+            t.YCoordinate = point.Longitude;
+            t.PatrolSize = 0;
+            t.PatrolDate = DateTime.Now;
+
+            string output = JsonConvert.SerializeObject(t);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://d2b608e3.ngrok.io/api/Police");
+            request.Method = "POST";
+
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(output);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+
+            dataStream.WriteAsync(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+        }
+
+    }
+}
---
+++ frontend/Helpersome/Whatever/Whatever/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Whatever")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Whatever")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15df4086-028f-46fb-a956-171748f1fcc0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
