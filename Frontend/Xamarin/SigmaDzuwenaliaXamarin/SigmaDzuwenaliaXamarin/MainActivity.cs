953b6ce5b4b4bb4bda06068196ec669c1732f5ab
senbar <barsen97@gmail.com>
2018-11-17 05:57:04 +0100

    initial commit on frontend

---
+++ Frontend/Xamarin/SigmaDzuwenaliaXamarin/SigmaDzuwenaliaXamarin/MainActivity.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Plugin.CurrentActivity;
+
+namespace SigmaDzuwenaliaXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private MapController mapController;
+
+        readonly string[] PermissionsLocation =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            mapController = new MapController();
+
+            //TODO permission management
+            while (CheckSelfPermission(PermissionsLocation[0]) == Permission.Denied ||
+                CheckSelfPermission(PermissionsLocation[1])==Permission.Denied)
+            {
+                RequestPermissions(PermissionsLocation, 1);
+            }
+
+
+
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(mapController);
+
+
+
+            //syf
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+        }
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private class SnackListener :AppCompatActivity, Android.Views.View.IOnClickListener
+        {
+            public void OnClick(View v)
+            {
+                MapController.MapState = MapState.PLACING_POLICE;
+            }
+        }
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Zaznacz patrol", Snackbar.LengthLong)
+                .SetAction("PATROL", new SnackListener()).Show();
+            
+        }
+
+
+    }
+
+
+}
---
+++ Frontend/Xamarin/SigmaDzuwenaliaXamarin/SigmaDzuwenaliaXamarin/MapController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using static Android.Gms.Maps.GoogleMap;
+
+namespace SigmaDzuwenaliaXamarin
+{
+    enum MapState { PLACING_POLICE, NULL };
+
+    class MapController : AppCompatActivity,IOnMapReadyCallback, IOnMapClickListener
+    {
+        static public MapState MapState= MapState.NULL;
+        private GoogleMap _googleMap;
+
+        public void OnMapClick(LatLng point)
+        {
+            var markerOption = new MarkerOptions().SetPosition(point);
+            markerOption.SetTitle("test");
+            var bmDescriptor = BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueRed);
+            switch (MapController.MapState)
+            {
+                case (MapState.PLACING_POLICE):
+                    //2130837627
+                    bmDescriptor = BitmapDescriptorFactory.FromBitmap(resizeMapIcons(2130837627, 24,24));
+                    
+                    break;
+
+                case (MapState.NULL):
+                    bmDescriptor = BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueYellow);
+                    break;
+            }
+            markerOption.SetIcon(bmDescriptor);
+            _googleMap.AddMarker(markerOption);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            //location layer on
+            googleMap.MyLocationEnabled = true;
+            googleMap.SetOnMapClickListener(this);
+
+            _googleMap = googleMap;
+        }
+
+
+        private Bitmap resizeMapIcons(int id, int width, int height)
+        {
+            var imageBitmap = BitmapFactory.DecodeResource(this.Application.Resources, id);
+            Bitmap resizedBitmap = Bitmap.CreateScaledBitmap(imageBitmap, width, height, false);
+            return resizedBitmap;
+        }
+    }
+}
